f86c53ab0ddfb627fde803e525a521e19a04ac9a
Kevin-Developer-B <kevin_breiter@web.de>
2026-04-28 17:34:44 +0200

    added surveys component

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FeedbackAndIdea } from './feedback-and-idea/feedback-and-idea';
+import { Surveys } from './surveys/surveys';
 
 @Component({
   selector: 'app-home',
-  imports: [FeedbackAndIdea],
+  imports: [FeedbackAndIdea, Surveys],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
