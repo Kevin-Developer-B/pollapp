a0865470c9a0c45e19f7fdc9ae98077cd0c036e5
Kevin-Developer-B <kevin_breiter@web.de>
2026-04-30 09:59:37 +0200

    edit surveylist and added dropdown menu

--- src/app/components/home/surveyslist/surveyslist.ts
+++ src/app/components/home/surveyslist/surveyslist.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-surveyslist',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './surveyslist.html',
   styleUrl: './surveyslist.scss',
 })
-export class Surveyslist {}
+
+
+export class Surveyslist {
+  selectedCategory: string = "all";
+  isDropdownOpen = true;
+
+  cardlist = [
+    {
+      catergory: "Team activities",
+      text: "Let’s Plan the Next Team Event Together",
+      day: "Ends in 1 Day"
+    },
+    {
+      catergory: "Health & Wellness",
+      text: "Fit & wellness survey!",
+      day: "Ends in 2 Day"
+    },
+    {
+      catergory: "Gaming & Entertainment",
+      text: "Gaming habits and favorite games!",
+      day: "Ends in 3 Day"
+    },
+  ]
+
+  filterSelection(item: string) {
+    this.selectedCategory = item
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+}
