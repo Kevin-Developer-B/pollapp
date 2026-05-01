05c6941aa86d5941cdc5db00736f57ca03e7df9f
Kevin-Developer-B <kevin_breiter@web.de>
2026-05-01 15:30:08 +0200

    edit dropdown menu

--- src/app/components/home/surveyslist/surveyslist.ts
+++ src/app/components/home/surveyslist/surveyslist.ts
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
 
 export class Surveyslist {
   selectedCategory: string = "all";
-  isDropdownOpen = true;
+  isDropdownOpen = false;
 
   cardlist = [
     {
