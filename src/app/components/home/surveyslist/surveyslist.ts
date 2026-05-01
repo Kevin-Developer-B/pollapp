dcbf6026776e6d24600794ac0f4088c8c38d5287
Kevin-Developer-B <kevin_breiter@web.de>
2026-05-01 18:02:00 +0200

    edit category dropdown indegrate filter

--- src/app/components/home/surveyslist/surveyslist.ts
+++ src/app/components/home/surveyslist/surveyslist.ts
@@ -10,37 +10,55 @@ import { CommonModule } from '@angular/common';
 
 
 export class Surveyslist {
-  selectedCategory: string = "active";
+  selectedActive: string = "active";
+  selectedCategory: string = "";
   isDropdownOpen = false;
   dropdownText = signal("")
 
-  cardlist = [
+  allCards = [
     {
       catergory: "Team activities",
       text: "Let’s Plan the Next Team Event Together",
-      day: "1"
+      day: "1",
+      stood: "past"
     },
     {
       catergory: "Health & Wellness",
       text: "Fit & wellness survey!",
-      day: "2"
+      day: "2",
+      stood: "active"
     },
     {
       catergory: "Gaming & Entertainment",
       text: "Gaming habits and favorite games!",
-      day: "3"
+      day: "3",
+      stood: "active"
     },
   ]
 
+  cardlist = [...this.allCards];
+
+  ngOnInit() {
+    this.filterSelection('active')
+  }
+
   filterSelection(item: string) {
+    this.selectedActive = item
+    let card = this.allCards.filter(card => card.stood == item)
+    if (card) this.cardlist = card
+  }
+
+  filterCategory(item: string) {
     this.selectedCategory = item
+    let card = this.allCards.filter(card => card.catergory == item)
+    if (card) this.cardlist = card
   }
 
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
   }
 
-  activeCategory() {
-
-  }
+  // setActiveCategory(name: string) {
+  //   let tmpCard = this.cardlist.find(card => card.catergory == name)
+  // }
 }
