95113ac50768ebbff5246cf2488d61bf1d7fc17f
Kevin-Developer-B <kevin_breiter@web.de>
2026-09-15 07:43:32 +0200

    edit filter active / past survey in surveylist.html

--- src/app/components/home/surveyslist/surveyslist.ts
+++ src/app/components/home/surveyslist/surveyslist.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropdownMenu } from '../../../services/dropdown_service';
 import { Surveys } from '../../../shared/services/surveys';
@@ -16,22 +16,46 @@ export class Surveyslist {
   surveyService = inject(Surveys);
   list = this.surveyService.surveyslist;
   dropdownMenu = inject(DropdownMenu);
-  selectedActive: string = "active";
-  selectedCategory: string = "";
+  selectedActive = signal("active");
+  selectedCategory = signal('');
 
-  cardlist = [...this.list()];
+  private getDaysLeft(date: string | Date): number {
+    const end = new Date(date);
+    const today = new Date();
+    const diff = end.getTime() - today.getTime();
+    return Math.ceil(diff / (1000 * 60 * 60 * 24));
+  }
+
+
+  cardlist = computed(() => {
+    const active = this.selectedActive();
+    const category = this.selectedCategory();
+    const surveys = this.list();
+
+    return surveys.filter(item => {
+      const days = this.getDaysLeft(item.date);
+
+      const activeCheck =
+        active === 'active' ? days > 0 : days <= 0;
+
+      const categoryCheck =
+        category === '' ? true : item.category === category;
+
+      return activeCheck && categoryCheck;
+    });
+  });
 
 
   filterSelection(item: string) {
-    this.selectedActive = item;
+    this.selectedActive.set(item);
   }
 
   filterCategory(item: string) {
-    this.selectedCategory = item;
+    this.selectedCategory.set(item);
   }
 
   resetFilter() {
-    this.selectedActive = 'active';
-    this.selectedCategory = '';
+    this.selectedCategory.set('');
+    this.dropdownMenu.dropdownText.set('');
   }
 }
